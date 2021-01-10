0d39fbae4aee14768f13b6ae0a38f806bffc21af
Halvor Smørvik <havlor@Halvors-Mac-mini.local>
2021-01-10 15:03:31 +0100

    Reading multiple files does not crash logview now

--- logview.py
+++ logview.py
@@ -99,7 +99,7 @@ def top_20(score):
     return top_array
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', action='store', dest='log_name', default="logs/CIFAR_random_samling_valida10-01-2021T13:24:43.json")
+parser.add_argument('-n', action='store', dest='log_name')
 parser.add_argument('-s', action='store', dest='search_string',
                     default='*')
 parser.add_argument('--tex', action='store', dest='produce_tex', default=False)
@@ -108,9 +108,9 @@ args = parser.parse_args()
 if args.log_name:
     log_opener = Log_open(args.log_name)
     log_opener.list_metadata()
-    (cm_array_test, cm_array_train) = conf_matrix_extract(log_opener.get_epochs())
-    (MCC_a, accuracy_a_test, f1_a) = calculate_scores(cm_array_test)
-    (MCC_a, accuracy_a_train, f1_a) = calculate_scores(cm_array_train)
+    (validation_confusion_matrix_array_test, validation_confusion_matrix_array_train) = conf_matrix_extract(log_opener.get_epochs())
+    (MCC_a, accuracy_a_test, f1_a) = calculate_scores(validation_confusion_matrix_array_test)
+    (MCC_a, accuracy_a_train, f1_a) = calculate_scores(validation_confusion_matrix_array_train)
 
     # plt.plot(MCC_a, label='MCC')
     plt.plot(accuracy_a_test, label='acc_test')
@@ -120,67 +120,71 @@ if args.log_name:
     plt.xlabel('epoch')
     plt.legend()
     plt.show()
-# else:
-#     log_files_names = read_log_names(args.search_string)
-#     print("all logfiles:", log_files_names)
-#     print()
-#     logs = []
-#     for name in log_files_names:
-#         logs.append([name, read_log(name)])
-#     acc_arr = []
-#     logs_without_trash_logs = []
-#     for log in logs:
-#         cm_array = conf_matrix_extract(log[1])
-#         if len(cm_array) < 100:
-#             print(log[0][5:], "has less than 100 epochs!")
-#             continue
-#         (MCC, acc, f1) = calculate_scores(cm_array)
-#         acc_arr.append(average_last_100(acc))
-#         logs_without_trash_logs.append(log)
-#     acc_sorted_logs = [x for _,x in sorted(zip(acc_arr,logs_without_trash_logs))]
-#     pair_matrix_acc = np.zeros((10,10))
-#     pair_matrix_acc_top = np.zeros((10,10))
-#     pair_matrix_test = np.zeros((10,10))
-#     print("average last 100:")
-#     for log in acc_sorted_logs:
-#         cm_array = conf_matrix_extract(log[1])
-#         (MCC, acc, f1) = calculate_scores(cm_array)
-#         av_acc_100 = average_last_100(acc)
-#         av_mcc_100 = average_last_100(MCC)
-#         print(log[0][5:],log[1][0])
-#         print("Last 100 acc:", av_acc_100, "MCC:", av_mcc_100)
-#         print("top 20 acc:", top_20(acc))
-#         print()
-#     avg_pma = []
-#     for x_pair in pair_matrix_acc:
-#         for y_pair in x_pair:
-#             if y_pair != 0:
-#                 avg_pma.append(y_pair)
-#     print("Average Acc pair matrix:", mean(avg_pma)*100)
-#     print("Accuracy over the last 100 epochs for pairs:")
-#     print (pd.DataFrame(pair_matrix_acc.T*100, index=cifar10_labels,
-#                         columns=cifar10_labels))
-#     average_dataframe = pd.DataFrame(pair_matrix_acc.T*100, index=cifar10_labels,
-#                         columns=cifar10_labels)
-#     if args.produce_tex:
-#         with open('average.tex', 'w') as tf:
-#             tf.write(average_dataframe.to_latex(float_format=lambda x: '%10.2f' % x))
-#     print()
-#     avg_pmat = []
-#     for x_pair in pair_matrix_acc_top:
-#         for y_pair in x_pair:
-#             if y_pair != 0:
-#                 avg_pmat.append(y_pair)
+else:
+    log_files_relative_path_names = read_log_names(args.search_string)
+    print("all logfiles:", log_files_relative_path_names)
+    print()
+    logs = []
+    for relative_path_name in log_files_relative_path_names:
+        log_opener = Log_open(relative_path_name)
+        logs.append([relative_path_name, log_opener])
+    acc_arr = []
+    logs_without_trash_logs = []
+    for log in logs:
+        validation_confusion_matrix_array, training_confusion_matrix_array = conf_matrix_extract(np.asarray(log[1].get_epochs()))
+        if len(validation_confusion_matrix_array) < 5:
+            print(log[0][5:], "has less than 100 epochs!")
+            continue
+        # print(validation_confusion_matrix_array)
+        (MCC, acc, f1) = calculate_scores(validation_confusion_matrix_array)
+        acc_arr.append(average_last_100(acc))
+        logs_without_trash_logs.append(log)
+    acc_sorted_logs = [x for _,x in sorted(zip(acc_arr,logs_without_trash_logs))]
+    pair_matrix_acc = np.zeros((10,10))
+    pair_matrix_acc_top = np.zeros((10,10))
+    pair_matrix_test = np.zeros((10,10))
+    print("average last 100:")
+    for log in acc_sorted_logs:
+        validation_confusion_matrix_array, training_confusion_matrix_array = conf_matrix_extract(log[1].get_epochs())
+        (MCC, acc, f1) = calculate_scores(validation_confusion_matrix_array)
+        av_acc_100 = average_last_100(acc)
+        av_mcc_100 = average_last_100(MCC)
+        print(log[0][5:],log[1].get_metadata())
+        print("Last 100 acc:", av_acc_100, "MCC:", av_mcc_100)
+        print("top 20 acc:", top_20(acc))
+        print()
 
-#     print("Average Acc top pair matrix:", mean(avg_pmat)*100)
-#     print("Top epoch accuracy for each pair:")
-#     print (pd.DataFrame(pair_matrix_acc_top.T*100, index=cifar10_labels,
-#                         columns=cifar10_labels))
+    # Pair matrix stuff, needs 45 log-pairs to work, old code:
+    # avg_pma = []
+    # for x_pair in pair_matrix_acc:
+    #     for y_pair in x_pair:
+    #         if y_pair != 0:
+    #             avg_pma.append(y_pair)
+    # print("Average Acc pair matrix:", mean(avg_pma)*100)
+    # print("Accuracy over the last 100 epochs for pairs:")
+    # print (pd.DataFrame(pair_matrix_acc.T*100, index=cifar10_labels,
+    #                     columns=cifar10_labels))
+    # average_dataframe = pd.DataFrame(pair_matrix_acc.T*100, index=cifar10_labels,
+    #                     columns=cifar10_labels)
+    # if args.produce_tex:
+    #     with open('average.tex', 'w') as tf:
+    #         tf.write(average_dataframe.to_latex(float_format=lambda x: '%10.2f' % x))
+    # print()
+    # avg_pmat = []
+    # for x_pair in pair_matrix_acc_top:
+    #     for y_pair in x_pair:
+    #         if y_pair != 0:
+    #             avg_pmat.append(y_pair)
 
-#     top_dataframe = pd.DataFrame(pair_matrix_acc_top.T*100, index=cifar10_labels,
-#                         columns=cifar10_labels)
-#     if args.produce_tex:
-#         with open('top.tex', 'w') as tf:
-#             tf.write(top_dataframe.to_latex(float_format=lambda x: '%10.2f' % x))
+    # print("Average Acc top pair matrix:", mean(avg_pmat)*100)
+    # print("Top epoch accuracy for each pair:")
+    # print (pd.DataFrame(pair_matrix_acc_top.T*100, index=cifar10_labels,
+    #                     columns=cifar10_labels))
+
+    # top_dataframe = pd.DataFrame(pair_matrix_acc_top.T*100, index=cifar10_labels,
+    #                     columns=cifar10_labels)
+    # if args.produce_tex:
+    #     with open('top.tex', 'w') as tf:
+    #         tf.write(top_dataframe.to_latex(float_format=lambda x: '%10.2f' % x))
 
 
