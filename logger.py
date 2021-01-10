19966df088e925a5a5b47164acd002cb67d13675
Halvor Smørvik <havlor@Halvors-Mac-mini.local>
2021-01-10 12:58:35 +0100

    Log opener updated to json format

--- logger.py
+++ logger.py
@@ -1,67 +1,51 @@
 from sklearn.metrics import confusion_matrix
 import numpy as np
 import time
-import pickle
 from datetime import datetime
 import os
 import json
-# for whole log: [[metadata], [epoch_nr, time_finished, [confusion_matrix]]]
-# metadata: [[name_x_train, name_y_train, name_x_val, name_y_val], [shape of all
-# lists]]
-# metadata is written once in the start, each epoch gets their own list with
-# add_epoch
-
 
 class Logger ():
     def __init__(self, name_dataset, ts_version, clauses, T, s, mask =
                  'no mask', comments = "NA comments"):
         self.metadata = { "name_dataset": name_dataset, "ts_version":ts_version, "clauses":clauses, "T":T, "s":s, "mask":mask, "comments" : comments}
         self.name_dataset = name_dataset[:-4]
-        self.frames = []
-        self.log = {}
-        self.log_open = []
+        self.epochs = []
         self.now = datetime.now()
-    def add_epoch(self, y_true_test, y_pred_test, y_true_train, y_pred_train):
-        self.frames.append( {"epoch_time" : time.time(), 
-            "validation_confusion_matrix" :  confusion_matrix(y_true_test, y_pred_test).tolist(), "training_confusion_matrix" : confusion_matrix(y_true_train, y_pred_train).tolist()})
+    def add_epoch(self, y_true_validation, y_pred_validation, y_true_train, y_pred_train):
+        self.epochs.append( {"epoch_time" : time.time(), 
+            "validation_confusion_matrix" :  confusion_matrix(y_true_validation, y_pred_validation).tolist(), 
+            "training_confusion_matrix" : confusion_matrix(y_true_train, y_pred_train).tolist()
+            }
+            )
 
     def make_log(self):
-        self.log = {"metadata" : self.metadata, "epochs" : self.frames}
+        self.full_log = {"metadata" : self.metadata, "epochs" : self.epochs}
 
     def save_log(self):
         log_folder = './logs'
         if os.path.isdir(log_folder) == False:
             os.mkdir(log_folder)
         dt_string = self.now.strftime("%d-%m-%YT%H:%M:%S")
-        self.make_log()
-        print(self.log)
         with open(os.path.join(log_folder, self.name_dataset+dt_string+'.json'), 'w') as f:
-            json.dump(self.log, f)
+            json.dump({"metadata" : self.metadata, "epochs" : self.epochs}, f)
 
 class Log_open():
-    def __init__(self):
-        self.log = []
+    def __init__(self, log_file_name = 'logs/test.json'):
         self.filename = ''
-    def open_log(self, log_file='test.pkl'):
-        self.filename = log_file
-        with open(log_file, 'rb') as f:
-            self.log = pickle.load(f)
+        self.filename = log_file_name
+        with open(log_file_name, 'rb') as f:
+            self.full_log = json.load(f)
+
     def list_metadata(self):
-        #print(self.log)
+        metadata = self.full_log["metadata"]
         print("Metadata for the log", self.filename)
-        print("Name:", self.log[0][0][0])
-        print("Tsetlin type:", self.log[0][0][1])
-        print("Clauses:", self.log[0][0][2])
-        print("T:", self.log[0][0][3])
-        print("s:", self.log[0][0][4])
-        print("Mask:", self.log[0][0][5])
+        print("Name:", metadata["name_dataset"])
+        print("Tsetlin type:", metadata["ts_version"])
+        print("Clauses:", metadata["clauses"])
+        print("T:", metadata["T"])
+        print("s:", metadata["s"])
+        print("Mask:", metadata["mask"])
 
-dataset = [[3,4],[5,6]]
-log = Logger("test_navn4_.pkl",  "test_tsetlin", 2000, 50, 10.0, (2,3))
-log.add_epoch([1,0,0,1],[1,0,1,1],[1,0,0,1],[1,0,0,1])
-log.add_epoch([1,0,0,1],[1,0,1,1],[1,0,0,1],[1,0,0,1])
-log.add_epoch([1,0,0,1],[1,0,1,1],[1,0,0,1],[1,0,0,1])
-log.add_epoch([1,0,0,1],[1,0,1,1],[1,0,0,1],[1,0,0,1])
-
-log.save_log()
-#log_opener = Log_open()
+    def get_epochs(self):
+        return self.full_log.epochs
